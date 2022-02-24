1afa437f71488b1e2ae105fe32078f39591a8cf6
PTRL481\Anusha.Medichelmi <anusha.m@xcubelabs.com>
2022-02-24 17:23:02 +0530

    This is a basic shooting game.

---
+++ Assets/BulletLauncher.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletLauncher : MonoBehaviour
+{
+
+    public GameObject bulletPrefab;
+    public int speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject tempBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            tempBullet.transform.position=transform.position;
+          //  Camera camera=GetComponentInChildren<Camera>();
+            tempBullet.GetComponent<Rigidbody>().velocity = Camera.main.transform.rotation*Vector3.forward * speed;
+           
+
+        }
+    }
+}
---
+++ Assets/CameraRotation.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+
+    public float rotationSpeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        transform.rotation = transform.rotation * Quaternion.Euler(0,mouseX*rotationSpeed,0);
+        Camera cam = GetComponentInChildren<Camera>();
+        cam.transform.localRotation = Quaternion.Euler(-mouseY, 0, 0)*cam.transform.localRotation;
+    }
+}
